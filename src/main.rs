a527b47036c516b5918909e054ac25ed920ea97c
Azathoth1729 <aa1203528272@gmail.com>
2022-05-17 19:54:18 +0800

    (feature): Testing

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,34 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+// test attribute
+#![feature(custom_test_frameworks)]
+#![test_runner(rust_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use rust_os::println;
 use core::panic::PanicInfo;
 
-mod macros;
-mod vga_buffer;
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
 
 /// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
-#[no_mangle] // don't mangle the name of this function
-pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
-
-    println!("Hello World{}", "!");
-    panic!("Some panic message");
-    loop {}
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info)
 }
