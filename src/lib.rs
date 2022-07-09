6a760dfd6e441588df6c166a5b976d905ec2fe77
Azathoth1729 <aa1203528272@gmail.com>
2022-07-09 22:13:03 +0800

    feat: Hardware Interrupts

--- src/lib.rs
+++ src/lib.rs
@@ -42,7 +42,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     serial_println!("[failed]\n");
     serial_println!("Error: {}\n", info);
     exit_qemu(QemuExitCode::Failed);
-    loop {}
+    hlt_loop();
 }
 
 #[cfg(test)]
@@ -70,6 +70,14 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 pub fn init() {
     gdt::init();
     interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
+}
+
+pub fn hlt_loop() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+    }
 }
 
 /// Entry point for `cargo test`
@@ -78,5 +86,5 @@ pub fn init() {
 pub extern "C" fn _start() -> ! {
     init();
     test_main();
-    loop {}
+    hlt_loop();
 }
--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ pub extern "C" fn _start() -> ! {
     test_main();
 
     println!("It did not crash!");
-    loop {}
+    ruos::hlt_loop();
 }
 
 /// This function is called on panic.
@@ -27,7 +27,7 @@ pub extern "C" fn _start() -> ! {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
-    loop {}
+    ruos::hlt_loop()
 }
 
 #[cfg(test)]
